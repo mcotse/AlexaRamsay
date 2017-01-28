945acc37e943d1ac129e220a854a86cb58167b62
Kevin Yang <k46yang@uwaterloo.ca>
2017-01-28 17:20:47 -0500

    Finished endpoints

--- app.py
+++ app.py
@@ -11,6 +11,56 @@ Session = sessionmaker(bind=engine, autoflush=True)
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/recipe", methods=['GET'])
+def get_recipe():
+    db_session = Session()
+    db_session.query(CurrentRecipe).delete()
+    db_session.query(CurrentInstruction).delete()
+
+    db_user = db_session.query(User).get(1)
+    recipe_id_set = None
+    for ingredient in db_user.user_ingredients:
+        db_ingredients = db_session.query(Ingredient).filter(Ingredient.name.like("%{}%".format(ingredient.name))).all()
+        tmp_recipe_set = set(x.recipe_id for x in db_ingredients)
+        if recipe_id_set is None:
+            recipe_id_set = tmp_recipe_set
+        else:
+            recipe_id_set = recipe_id_set & tmp_recipe_set
+
+    if len(recipe_id_set) > 0:
+        recipe_id = recipe_id_set.pop()
+        db_recipe = db_session.query(Recipe).get(recipe_id)
+        db_current_recipe = CurrentRecipe()
+        db_current_recipe.recipe_id = recipe_id
+        db_current_recipe.status_id = 1
+        db_session.add(db_current_recipe)
+
+        for recipe_instruction in db_recipe.recipe_instructions:
+            db_current_instruction = CurrentInstruction()
+            db_current_instruction.instruction_id = recipe_instruction.id
+            db_current_instruction.status_id = 1
+            db_session.add(db_current_instruction)
+
+        db_session.commit()
+        return jsonify({"recipe_name": db_recipe.name})
+    else:
+        return jsonify({"error": "Could not find a valid recipe"}, status_code=400)
+
+
+@app.route("/step", methods=['GET'])
+def get_step():
+    db_session = Session()
+
+    db_current_recipe = db_session.query(CurrentRecipe).first()
+
+    db_current_instruction = db_session.query(CurrentInstruction).filter_by(status_id=1).first()
+
+    if db_current_instruction is None:
+        db_current_recipe.status_id = 3
+        db_session.commit()
+        return jsonify({"instruction": "You are done."})
+
+    db_current_recipe.status_id = 2
+    db_current_instruction.status_id = 3
+    db_session.commit()
+    return jsonify({"instruction": db_current_instruction.instruction.text})
--- db/models.py
+++ db/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, DECIMAL, Boolean, TEXT, TIMESTAMP
 from datetime import datetime
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -44,9 +45,74 @@ class Instruction(Base):
     __tablename__ = "instruction"
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    recipe_id = Column(Integer)
+    recipe_id = Column(Integer, ForeignKey("recipe.id"))
     step_number = Column(Integer)
     text = Column(TEXT)
+
+    recipe = relationship("Recipe", backref="recipe_instructions")
+
+
+    def to_dict(self):
+        return dict(
+        )
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    email = Column(String(255))
+
+    def to_dict(self):
+        return dict(
+        )
+
+
+class UserIngredients(Base):
+    __tablename__ = "user_ingredients"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    name = Column(String(255))
+
+    user = relationship("User", backref="user_ingredients")
+
+    def to_dict(self):
+        return dict(
+        )
+
+
+class CurrentRecipe(Base):
+    __tablename__ = "current_recipe"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    recipe_id = Column(Integer)
+    status_id = Column(Integer)
+
+    def to_dict(self):
+        return dict(
+        )
+
+
+class CurrentInstruction(Base):
+    __tablename__ = "current_instruction"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    instruction_id = Column(Integer, ForeignKey("instruction.id"))
+    status_id = Column(Integer)
+
+    instruction = relationship("Instruction")
+
+    def to_dict(self):
+        return dict(
+        )
+
+
+class Status(Base):
+    __tablename__ = "status"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(45))
 
     def to_dict(self):
         return dict(
