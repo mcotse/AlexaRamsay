48db0791be9f4bc192515c6f6ed3dff181dd7168
Kevin Yang <k46yang@uwaterloo.ca>
2017-01-28 16:23:11 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Ramsay',
+      version='0.1',
+      description='Alexa Chef Instructor',
+      author='Kevin Yang',
+      author_email='k46yang@uwaterloo.ca',
+      url='deltahacks.com',
+      packages=[],
+     )
