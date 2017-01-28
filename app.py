8c4632c3d769df63abdb2045743a8aaf11de5183
Kevin Yang <k46yang@uwaterloo.ca>
2017-01-28 16:25:26 -0500

    Added charset to db connection string

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from db.models import *
 
 engine = sa.create_engine(
-    'mysql+mysqldb://ramsay:ramsay123@ramsay.cn8pndyiytrv.us-east-1.rds.amazonaws.com:3306/Ramsay')
+    'mysql+mysqldb://ramsay:ramsay123@ramsay.cn8pndyiytrv.us-east-1.rds.amazonaws.com:3306/Ramsay?charset=utf8')
 Session = sessionmaker(bind=engine, autoflush=True)
 
 app = Flask(__name__)
