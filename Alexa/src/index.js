768a0b7b3e77e010dfb9ccc7c735c365209a2c2e
Matthew Tse <mtse9779@gmail.com>
2017-01-28 22:50:00 -0500

    Use alexa-sdk, full rewrite

--- Alexa/src/index.js
+++ Alexa/src/index.js
@@ -1,88 +1,82 @@
-'use strict';
+var Alexa = require('alexa-sdk');
 var request = require('request');
-/**
- * App ID for the skill
- */
-var APP_ID = process.env.app_id; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
-/**
- * The AlexaSkill prototype and helper functions
- */
-var AlexaSkill = require('./AlexaSkill');
 
-var Ramsey = function () {
-    AlexaSkill.call(this, APP_ID);
-};
+var handlers = {
 
-// Extend AlexaSkill
-Ramsey.prototype = Object.create(AlexaSkill.prototype);
-Ramsey.prototype.constructor = Ramsey;
-
-Ramsey.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("Ramsey onSessionStarted requestId: " + sessionStartedRequest.requestId
-        + ", sessionId: " + session.sessionId);
-    // any initialization logic goes here
-};
-
-Ramsey.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("Ramsey onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-    var speechOutput = "Welcome to Ramsey. I can guide you through recipes based on your dietitians perscription";
-    var repromptText = "ask for a recipe";
-    response.ask(speechOutput, repromptText);
-};
-
-Ramsey.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("Ramsey onSessionEnded requestId: " + sessionEndedRequest.requestId
-        + ", sessionId: " + session.sessionId);
-    // any cleanup logic goes here
-};
-
-Ramsey.prototype.intentHandlers = {
-    // register custom intent handlers
-    "GetNextRecipe": function (intent, session, response) {
-      request(process.env.uri + "/get_next_recipe", function(err, res, body){
+    "LaunchRequest": function () {
+      var speechOutput = "Welcome to Ramsay. I can guide you through recipes based on your dietitians perscription";
+      this.emit(':tell',speechOutput);
+    },
+    'Unhandled': function() {
+      this.emit(':ask', 'Nooooooo we have a problem');
+    },
+    "NextRecipeIntent": function () {
+      var self = this;
+      request(process.env.uri + "/recipe", function(err, res, body){
         if (!err && res.statusCode == 200){
           body = JSON.parse(body);
-          var dishName = body.name;
-          response.ask('would you like to make ' + dishName);
-          response.tellWithCard(dishName);
+          var cardTitle = 'Recipe Name';
+          var cardContent = body.recipe_name;
+          var speechOutput = 'would you like to make ' + body.recipe_name;
+          self.emit(':askWithCard', speechOutput, cardTitle, cardContent)
         }
       });
     },
-    "GetNextStep": function (intent, session, response) {
-      request(process.env.uri + "/get_next_step", function(err, res, body){
+    "NextStepIntent": function () {
+      var self = this;
+      request(process.env.uri + "/step", function(err, res, body){
         if (!err && res.statusCode == 200){
           body = JSON.parse(body);
-          response.ask(body.step);
-          response.tellWithCard(body.step);
+          var cardTitle = 'Step procedure';
+          var cardContent = body.instruction;
+          var speechOutput = body.instruction;
+          self.emit(':tellWithCard', speechOutput, cardTitle, cardContent)
         }
       });
     },
-    "AMAZON.HelpIntent": function (intent, session, response) {
-        response.ask("Ask for next steps or next recipe");
-    },
-    "AMAZON.YesIntent": function (intent, session, response) {
-      request(process.env.uri + "/get_next_recipe", function(err, res, body){
-        if (!err && res.statusCode == 200){
-          body = JSON.parse(body);
-          response.ask(body.name);
-          response.tellWithCard(body.name);
+    "IngredientsToRecipeIntent": function() {
+      var self = this;
+      var ingredients = this.event.request.intent.slots;
+      var ingr_array = [];
+      for (var key in ingredients) {
+        for (var name in ingredients[key]){
+          if (name == 'value'){
+            ingr_array.push(ingredients[key][name])
+          }
         }
-      });
-    },
-    "AMAZON.NoIntent": function (intent, session, response) {
-      request(process.env.uri + "/get_next_step", function(err, res, body){
+      };
+      var options = {
+        uri: process.env.uri + "/recipe",
+        method: "POST",
+        json: { "ingredients": ingr_array }
+      };
+      // console.log(options);
+      request(options, function(err, res, body){
         if (!err && res.statusCode == 200){
-          body = JSON.parse(body);
-          response.ask('would you like to make ' + body.step);
-          response.tellWithCard(body.step);
+          // body = JSON.parse(body);
+          console.log(body);
+          var cardTitle = 'Recipe Name';
+          var cardContent = body.recipe_name;
+          var speechOutput = 'would you like to make ' + body.recipe_name;
+          self.emit(':askWithCard', speechOutput, cardTitle, cardContent)
         }
-      });
+      })
+    },
+  //   { i_two: { name: 'i_two', value: 'rice' },
+  // i_one: { name: 'i_one', value: 'chicken' },
+  // i_three: { name: 'i_three' },
+  // i_four: { name: 'i_four' } }
+    "AMAZON.YesIntent": function () {
+      this.emit('NextStepIntent');
+    },
+    "AMAZON.NoIntent": function () {
+      this.emit('NextRecipeIntent');
     }
 };
 
-// Create the handler that responds to the Alexa Request.
-exports.handler = function (event, context) {
-    // Create an instance of the Ramsey skill.
-    var ramsey = new Ramsey();
-    ramsey.execute(event, context);
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = process.env.app_id;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
