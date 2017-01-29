9d7b4e1e63ed0bc2c17b4d3a5b1142bb1be26918
Matthew Tse <mtse9779@gmail.com>
2017-01-29 01:33:09 -0500

    Add start over, last step and current recipe intents

--- Alexa/src/index.js
+++ Alexa/src/index.js
@@ -1,6 +1,7 @@
 var Alexa = require('alexa-sdk');
 var request = require('request');
 
+var url = process.env.uri;
 var handlers = {
 
     "LaunchRequest": function () {
@@ -12,7 +13,7 @@ var handlers = {
     },
     "NextRecipeIntent": function () {
       var self = this;
-      request(process.env.uri + "/recipe", function(err, res, body){
+      request(url + "/recipe", function(err, res, body){
         if (!err && res.statusCode == 200){
           body = JSON.parse(body);
           var cardTitle = 'Recipe Name';
@@ -20,19 +21,66 @@ var handlers = {
           var speechOutput = 'would you like to make ' + body.recipe_name;
           self.emit(':askWithCard', speechOutput, cardTitle, cardContent)
         } else {
-          self.emit(':ask', 'I could not find a recipe, would you like to try again');
+          self.emit(':tell', 'I could not find a recipe, please try again');
+        }
+      });
+    },
+    "CurrentRecipeIntent": function () {
+      var self = this;
+      request(url + "/current_recipe", function(err, res, body){
+        if (!err && res.statusCode == 200){
+          body = JSON.parse(body);
+          var cardTitle = 'Recipe Name';
+          var cardContent = body.recipe_name;
+          var speechOutput = 'the current recipe is ' + body.recipe_name;
+          self.emit(':tellWithCard', speechOutput, cardTitle, cardContent)
+        } else {
+          self.emit(':tell', 'I could not find the current recipe, please try again');
         }
       });
     },
     "NextStepIntent": function () {
       var self = this;
-      request(process.env.uri + "/step", function(err, res, body){
+      request(url + "/step", function(err, res, body){
         if (!err && res.statusCode == 200){
           body = JSON.parse(body);
           var cardTitle = 'Step procedure';
           var cardContent = body.instruction;
           var speechOutput = body.instruction;
           self.emit(':tellWithCard', speechOutput, cardTitle, cardContent)
+        }
+      });
+    },
+    "LastStepIntent": function () {
+      var self = this;
+      request(url + "/previous_step", function(err, res, body){
+        if (!err && res.statusCode == 200){
+          body = JSON.parse(body);
+          var cardTitle = 'Step procedure';
+          var cardContent = body.instruction;
+          var speechOutput = body.instruction;
+          self.emit(':tellWithCard', speechOutput, cardTitle, cardContent)
+        }
+      });
+    },
+    "StartOverStepIntent": function () {
+      var self = this;
+      request(url + "/current_recipe", function(err, res, body){
+        if (!err && res.statusCode == 200){
+          body = JSON.parse(body);
+          var cardTitle = 'Recipe Name';
+          var recipeName = body.recipe_name;
+          request(url + "/first_step", function(err, res, body){
+            if (!err && res.statusCode == 200){
+              console.log('hi');
+              body = JSON.parse(body);
+              var cardContent = body.instruction;
+              var speechOutput = 'starting over with the recipe ' + recipeName + '. ' +  body.instruction;
+              self.emit(':tellWithCard', speechOutput, cardTitle, cardContent)
+            }
+          });
+        } else {
+          self.emit(':tell', 'I could not find the current recipe, please try again');
         }
       });
     },
@@ -48,7 +96,7 @@ var handlers = {
         }
       };
       var options = {
-        uri: process.env.uri + "/recipe",
+        uri: url + "/recipe",
         method: "POST",
         json: { "ingredients": ingr_array }
       };
