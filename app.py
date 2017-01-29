a7cf6f91e8cf40c9a9062f83e41776a183ebb11a
Kevin Yang <k46yang@uwaterloo.ca>
2017-01-28 22:21:08 -0500

    Another error check

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_recipe():
             db_current_user_ingredient.user_ingredient_id = db_user_ingredient.id
             db_session.add(db_current_user_ingredient)
 
-        if len(recipe_id_set) > 0:
+        if recipe_id_set is not None and len(recipe_id_set) > 0:
             recipe_id = random.choice(list(recipe_id_set))
             db_recipe = db_session.query(Recipe).get(recipe_id)
             db_current_recipe = CurrentRecipe()
